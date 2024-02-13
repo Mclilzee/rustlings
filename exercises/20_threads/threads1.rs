223663966ae5bc25c85d9961b7eaa972d923b552
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-02-13 14:51:57 +0100

    Update thread 1 functionaly

--- exercises/20_threads/threads1.rs
+++ exercises/20_threads/threads1.rs
@@ -12,20 +12,18 @@ use std::thread;
 use std::time::{Duration, Instant};
 
 fn main() {
-    let mut handles = vec![];
-    for i in 0..10 {
-        handles.push(thread::spawn(move || {
-            let start = Instant::now();
-            thread::sleep(Duration::from_millis(250));
-            println!("thread {} is complete", i);
-            start.elapsed().as_millis()
-        }));
-    }
-
-    let mut results: Vec<u128> = handles
-        .into_iter()
-        .map(|handle| handle.join().unwrap())
-        .collect();
+    let results = (0..10)
+        .map(|i| {
+            thread::spawn(move || {
+                let start = Instant::now();
+                thread::sleep(Duration::from_millis(250));
+                println!("thread {} is complete", i);
+                start.elapsed().as_millis()
+            })
+            .join()
+            .unwrap()
+        })
+        .collect::<Vec<u128>>();
 
     if results.len() != 10 {
         panic!("Oh no! All the spawned threads did not finish!");
